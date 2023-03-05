9cc4d165528c369a41b958d888ba41e92e969dd6
Hong Bin Zhu <124533442+touch186@users.noreply.github.com>
2023-03-04 23:59:25 -0600

    Delete javascript directory

--- javascript/javascript.js
+++
@@ -1,9 +0,0 @@
-function getSelectionText() {
-    var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
-    console.log(text);
-}
