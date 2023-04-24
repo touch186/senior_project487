d6dfbc7a4a8547df0d1bf68488facf325da90541
Hong Bin Zhu <124533442+touch186@users.noreply.github.com>
2023-04-24 15:00:00 -0500

    Add files via upload

---
+++ admin/admin.js
@@ -0,0 +1,73 @@
+fetch('/get-files?folder=uploads')
+  .then(response => response.json())
+  .then(data => {
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i]);
+  }
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+
+fetch('/get-files?folder=uploads')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to get file list');
+    }
+    return response.json();
+  })
+  .then(files => {
+    files.forEach(file => {
+      const deleteBtn = document.createElement('button');
+      deleteBtn.innerText = `Delete ${file}`;
+      deleteBtn.addEventListener('click', () => {
+        deleteFile(file);
+      });
+      var deleteBtnsContainer = document.getElementById("delete-buttons-container");
+      deleteBtnsContainer.appendChild(deleteBtn);
+    });
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+function deleteFile(fileName) {
+  const url = `/deleteFile/${fileName}`;
+
+  fetch(url, {
+    method: 'DELETE',
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to delete file');
+      }
+      console.log(`File ${fileName} deleted successfully`);
+      const deleteBtn = document.querySelector(`button[data-file="${fileName}"]`);
+      alert("File deleted");
+      window.location.reload();
+      deleteBtn.remove();
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+const exportButton = document.getElementById('export-button');
+exportButton.addEventListener('click', function() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', '/export');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.responseType = 'blob';
+  xhr.onload = function() {
+    const url = window.URL.createObjectURL(xhr.response);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'data.csv';
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+  };
+  xhr.send();
+});
+
